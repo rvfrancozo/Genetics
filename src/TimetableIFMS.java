8691b25e6f26ec65ad483b642ab8e43caf079c93
Rafael Françozo <rvfrancozo@gmail.com>
2022-05-15 19:51:23 -0400

    up

--- src/TimetableIFMS.java
+++ src/TimetableIFMS.java
@@ -1,7 +1,7 @@
 import Util.Timetable;
 
 public class TimetableIFMS {
-//  token para o git rvfrancozo
+//  token para o git 
 //	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
 	
 	public static void main(String[] args) {

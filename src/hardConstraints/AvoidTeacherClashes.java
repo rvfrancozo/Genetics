c8360db2e0e76e7765f7e970e8017283553d2d2f
leonardo-felipe-sanchez <felipinhosanchez77@gmail.com>
2022-05-31 17:33:50 -0300

    o ogro despertou

--- src/hardConstraints/AvoidTeacherClashes.java
+++ src/hardConstraints/AvoidTeacherClashes.java
@@ -12,39 +12,47 @@ public class AvoidTeacherClashes implements Score {
 	@Override
 	public int calculator(int[] chromosome) {
 		
-		
 		ArrayList<DataModel> data = new DataControl().loader("data.csv");
 		
 		String teacher = data.get(0).getTeacher();
 		
 		int score = 0;
-	
-		
 			
 			for(int j = 0; j < chromosome.length; j+=5) {
 				
 				int hour_id = chromosome[j+1]; 
 				int day_id = chromosome[j+2];
-				int duration = chromosome[j+3]; 
 				
+				int classroom_id = chromosome[j+4];
 				
 				for(int i = 5; i < chromosome.length; i+=5) {
 					
 					int hour_id2 = chromosome[i+1]; 
 					int day_id2 = chromosome[i+2];
 					int duration2 = chromosome[i+3]; 
+					int classroom_id2 = chromosome[i+4];
 					
 					if(data.get(i).getTeacher().equals(data.get(j).getTeacher()) ) {
 						++score;
-				
+						
 						if(hour_id == hour_id2) {
 							score++;
+							if(hour_id > 10) {
+								score++;
+							}
 						}
 						
 						if(day_id == day_id2) {
 							score++;
+								
 						}
 						
+						if(classroom_id2 == classroom_id) {
+							score++;
+							if(classroom_id > 13) {
+								score++;
+							}
+						}								
 						if(hour_id2 <= 6) {
 							//manhã 
 							if(hour_id2 + duration2 > 6)
@@ -58,22 +66,15 @@ public class AvoidTeacherClashes implements Score {
 							if(hour_id2 + duration2 > 18)
 								++score;
 						}
-
-						
 				}
 					teacher = data.get(i).getTeacher();
 			}
 			}
-		
-		
-		System.out.println(teacher);
-		
+		System.out.println(teacher);	
 		return score + next.calculator(chromosome);
 	}
-
 	@Override
 	public void setNext(Score next) {
 		this.next = next;
 	}
-
 }

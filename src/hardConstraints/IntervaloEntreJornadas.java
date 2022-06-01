04f60ee64308d0cfe4cd00e34d3dfaa06bbb237f
ELQUELEY EVIN BARBOSA DA COSTA  TnolADS <04744605257@laboratorio.ifms.br>
2022-05-31 20:40:17 -0400

    atualizacao

---
+++ src/hardConstraints/IntervaloEntreJornadas.java
@@ -0,0 +1,33 @@
+package hardConstraints;
+
+import java.util.ArrayList;
+
+import dataLoader.DataControl;
+import dataLoader.DataModel;
+
+public class IntervaloEntreJornadas implements Score {
+	private Score next;
+	int score;
+	ArrayList<DataModel> data = new DataControl().loader("data.csv");
+
+	@Override
+	public int calculator(int[] chromosome) {
+		// TODO Auto-generated method stub
+		
+		for(int i = 0; i <= chromosome.length; i+=5) {
+			for(int j = 5; j < chromosome.length; j+=5) {
+				int time = chromosome[j+1];
+				if(time == 18 && time <= 2 ) 
+					++score;
+			}
+		}
+		
+		return score+next.calculator(chromosome);
+	}
+
+	@Override
+	public void setNext(Score next) {
+		// TODO Auto-generated method stub
+		
+	}
+}
